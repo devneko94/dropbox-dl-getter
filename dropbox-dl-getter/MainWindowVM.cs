9b5088fad48f61a2d396454023b60a8cb8030c61
devneko <m.goto.dev@gmail.com>
2024-07-19 10:53:03 +0900

    プロジェクト ファイルを追加します。

---
+++ dropbox-dl-getter/MainWindowVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace dropbox_dl_getter
+{
+    internal class MainWindowVM : BindableBase
+    {
+        private string _originalUrl = "";
+
+        public string OriginalUrl {
+            get => _originalUrl;
+            set
+            {
+                if (_originalUrl != value)
+                {
+                    _originalUrl = value;
+                    OnPropertyChanged();
+                    OnOriginalUrlChanged();
+                }
+            }
+        }
+
+        private string _convertedUrl = "";
+
+        public string ConvertedUrl {
+            get => _convertedUrl;
+            set
+            {
+                if (_convertedUrl != value)
+                {
+                    _convertedUrl = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void OnOriginalUrlChanged()
+        {
+            if (OriginalUrl == "")
+            {
+                return;
+            }
+
+            ConvertedUrl = Regex.Replace(OriginalUrl.Replace("www.dropbox.com", "dl.dropboxusercontent.com"), @"[\?\&]dl=0", "");
+        }
+    }
+}
